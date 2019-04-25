532043095c17686da8296227c6e4ee5e5e1e35a3
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-04-25 12:20:40 -0300

    Atualizacao do projeto

--- PanFASE/View/TelaFuncionario.cs
+++ PanFASE/View/TelaFuncionario.cs
@@ -39,15 +39,5 @@ namespace PanFase
                 MessageBox.Show(this, "Este campo precisa ser preenchido para que o cadastro seja efetuado!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
-
-        private void btnFeichar_Click(Object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void btnMininizar_Click(Object sender, EventArgs e)
-        {
-            WindowState = FormWindowState.Minimized;
-        }
     }
 }
